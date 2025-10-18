503fee7d8a5536b803ee14995572497936e04a66
joyvin <joymendonca710@gmail.com>
2025-10-18 17:13:50 +0530

    FInal changes

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+export const nextConfig = {
   reactStrictMode: false,
   eslint: {
     ignoreDuringBuilds: true, // ignores ESLint errors during build
